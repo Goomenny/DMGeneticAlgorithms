41cba783eedf60682f41cdab8c1da5c039dfad95
Данила <goomenny@gmail.com>
2019-03-24 00:27:27 +0700

    cargo representation is finished

--- binary_string.py
+++ binary_string.py
@@ -30,9 +30,7 @@ class binary_string:
         return str(self.get_result())
 
     def copy(self,other):
-        for x in self.cargo:
-
-            self.cargo[x] = other.cargo[x].copy()
+        self.cargo = other.cargo.copy()
         self.fitness = other.fitness
 
     def calculate_result(self):
@@ -40,13 +38,6 @@ class binary_string:
             summa = sum(self.cargo[n[1]-n[0]:n[1]] * 2 ** np.arange(n[0]))
             self.dec[dim] = self.bounds[dim][0]+summa*self.E[dim]*0.1
 
-    def calculate_result2(self):
-        for dim,genotype in self.cargo.items():
-            summa = 0
-            for i,gen in enumerate(genotype):
-                summa += 2**i*int(gen)
-            # summa = sum(genotype * 2 ** np.arange(len(genotype)))
-            self.dec[dim] = self.bounds[dim][0]+summa*self.E[dim]*0.1
 
     def get_result(self):
         self.calculate_result()
@@ -55,7 +46,7 @@ class binary_string:
     def calculate_fitness(self, obj_func):
 
         self.fitness = 1 / (1 + obj_func(self.get_result))
-        self.changed = True
+        self.changed = False
 
     def mutate(self, mutation_type="standard", probability=None):
 
@@ -67,35 +58,14 @@ class binary_string:
             elif mutation_type is "strong":
                 probability = 5 / self.size
 
-        mask = np.random.random_sample((self.size,)) < probability
-        self.cargo[np.logical_and(mask, self.cargo == 1)] = 0
-        self.cargo[np.logical_and(mask, self.cargo == 0)] = 1
-        # for i in range(self.size):
-        #     if rn.random() < probability:
-        #         self.changed = True
-        #         if self.cargo[i] == 0:
-        #             self.cargo[i] = 1
-        #         else:
-        #             self.cargo[i] = 0
+        mask = np.random.random((self.size,)) < probability
+        mask_0 = np.logical_and(mask, self.cargo == 0)
+        mask_1 = np.logical_and(mask, self.cargo == 1)
+        self.cargo[mask_0] = 1
+        self.cargo[mask_1] = 0
+        self.changed = True
 
-    def mutate2(self, mutation_type="standard", probability=None):
 
-        if not probability:
-            if mutation_type is "weak":
-                probability = 1/(5*self.size)
-            elif mutation_type is "standard":
-                probability = 1 / self.size
-            elif mutation_type is "strong":
-                probability = 5 / self.size
-
-        for dim, genotype in self.cargo.items():
-            for i, gen in enumerate(genotype):
-                if rn.random() < probability:
-                    self.changed = True
-                    if self.cargo[dim][i] == 0:
-                        self.cargo[dim][i] = 1
-                    else:
-                        self.cargo[dim][i] = 0
 
     def crossover(self, other, crossover_type="two_point",self_copy = None):
         if not self_copy:
@@ -104,46 +74,19 @@ class binary_string:
 
         if crossover_type is "standard":
             mask = np.random.randint(2, size=self.size)
-            self_copy.cargo[mask] = other.cargo[mask]
-            # for i in range(self.size):
-            #     if rn.random() < 0.5:
-            #         self_copy.cargo[i] = other.cargo[i]
-            #     else:
-            #         self_copy.cargo[i] = self.cargo[i]
+            self_copy.cargo = np.where(mask, self.cargo, other.cargo)
+
         elif crossover_type is "one_point":
             point_var = rn.randint(0,self.size-1)
-            flag = False
-            for i in range(self.size):
-                if point_var == i:
-                    flag = True
-                elif flag:
-                    self_copy.cargo[i] = other.cargo[i]
-                else:
-                    self_copy.cargo[i] = self.cargo[i]
+            self_copy.cargo = np.concatenate((self.cargo[:point_var], other.cargo[point_var:]))
+
 
         elif crossover_type is "two_point":
-            point_var = rn.randint(0,self.size-1)
+            point_var = rn.sample(range(self.size-2), k=2)
+            point_var.sort()
+            point_var = [x+1 for x in point_var]
+            self_copy.cargo = np.concatenate((self.cargo[:point_var[0]], other.cargo[point_var[0]:point_var[1]],self.cargo[point_var[1]:]))
 
-
-            while True:
-                second_point_var = rn.randint(0,self.size-1)
-                if point_var != second_point_var:
-                    break
-
-            flag = False
-            second_flag = False
-            for i in range(self.size):
-                if point_var == i  or second_point_var == i:
-
-                    if not flag:
-                        flag = True
-                    elif not second_flag:
-                        second_flag = True
-
-                if flag and not second_flag:
-                    self_copy.cargo[i] = other.cargo[i]
-                else:
-                    self_copy.cargo[i] = self.cargo[i]
         elif crossover_type is "empty":
             self_copy = rn.choice([self_copy,copy.deepcopy(other)])
 
@@ -153,74 +96,3 @@ class binary_string:
 
         return self_copy
 
-    def crossover2(self, other, crossover_type="two_point",self_copy = None):
-        if not self_copy:
-            self_copy = copy.deepcopy(self)
-        self_copy.fitness=self.fitness
-
-        if crossover_type is "standard":
-            for dim, genotype in self.cargo.items():
-                for i, gen in enumerate(genotype):
-                    if rn.random() < 0.5:
-                        self_copy.cargo[dim][i] = other.cargo[dim][i]
-                    else:
-                        self_copy.cargo[dim][i] = self.cargo[dim][i]
-        elif crossover_type is "one_point":
-            point_var = rn.choice(list(self.cargo.keys()))
-            point_gen = rn.choice([i+1 for i in range(len(self.cargo[point_var])-1)])
-            flag = False
-            for dim, genotype in self.cargo.items():
-                for i, gen in enumerate(genotype):
-                    if point_var is dim and point_gen is i:
-                        flag = True
-                    elif flag:
-                        self_copy.cargo[dim][i] = other.cargo[dim][i]
-                    else:
-                        self_copy.cargo[dim][i] = self.cargo[dim][i]
-
-        elif crossover_type is "two_point":
-            point_var = rn.choice(list(self.cargo.keys()))
-            point_gen = rn.choice([i + 1 for i in range(len(self.cargo[point_var]) - 1)])
-
-            while True:
-                second_point_var = rn.choice(list(self.cargo.keys()))
-                second_point_gen = rn.choice([i + 1 for i in range(len(self.cargo[second_point_var]) - 1)])
-                if point_var is not second_point_var or second_point_gen is not point_gen:
-                    break
-
-            flag = False
-            second_flag = False
-            for dim, genotype in self.cargo.items():
-                for i, gen in enumerate(genotype):
-                    if point_var is dim and point_gen is i or second_point_var is dim and second_point_gen is i:
-
-                        if flag:
-                            second_flag = True
-                            flag = False
-                        elif not flag: flag = True
-
-                    if flag:
-                        self_copy.cargo[dim][i] = other.cargo[dim][i]
-                    else:
-                        self_copy.cargo[dim][i] = self.cargo[dim][i]
-        elif crossover_type is "empty":
-            self_copy = rn.choice([self_copy,copy.deepcopy(other)])
-
-        for var,val in self.cargo.items():
-            if (val != self_copy.cargo[var]).all():
-                self_copy.changed = True
-                break
-
-        return self_copy
-
-if __name__ == "__main__":
-    import random
-    random.seed(64)
-    b1 = binary_string(bounds=[[-1,1]])
-    b2 = binary_string(bounds=[[-1, 1]])
-    print(b1.cargo)
-    print(b2.cargo)
-    b1= b1.crossover(b2,"standard")
-    print(b1.cargo)
-    b1.mutate()
-    print(b1.cargo)
