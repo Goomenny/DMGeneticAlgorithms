34c7554c18f6422288ad49b78ff934c4fd989b1b
Данила <goomenny@gmail.com>
2019-03-31 22:16:23 +0700

    zakharov 10d 1500300

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     # gp = GeneticProgramming(objective_function=objective_function,variables=variables)
 
     test =  tests.Test()
-    dims = [7]
+    dims = [10]
     #allfuncnames = tfunc.funcnames_minus()
     #allfuncs = [problem(func) for func in tfunc.allfuncs_minus()]
 
